4ee1851e4bbb3f82f916341811582e2ac7f973d8
unknown <zanbagh.mersad@gmail.com>
2020-03-13 12:04:26 -0700

    V1.01

--- Car.java
+++ Car.java
@@ -5,28 +5,36 @@ import java.time.*;
 class Car{
 
 public static void main(String[] args) {
+  
+  //creating scanner object for user input
   Scanner myObj = new Scanner(System.in);
   System.out.println("Enter Name, Age, Salary");
   String name = myObj.nextLine();
   Integer age = myObj.nextInt();
   Double salary = myObj.nextDouble();
+
+  // Print Local time using myTime Object
   LocalDate myTime = LocalDate.now();
 
-
-
+  //Print user input & time
   System.out.println("Hello, Name " + name);
   System.out.println("Hello, Age " + age);  
   System.out.println("Hello, Salary " + salary);
   System.out.println(myTime);
+  
+  //close object
   myObj.close();
 
-
+  // Creating arrayList object & add items
   ArrayList<String> cars = new ArrayList<String>();
   cars.add("Volvo");
   cars.add("Benzi");
   cars.add("Audi");
-
+  
+  //print objects in array
   System.out.println(cars);
+  
+  //upper case item 2 in array & print
   String item = cars.get(2).toUpperCase();
   System.out.println(item);
 
